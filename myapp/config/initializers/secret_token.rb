75bad2e1959c3b2036964be71b1e36ae5e129cbd
Bryan Arndorfer <barndorfer@Bryan-Arndorfers-MacBook.local>
2013-10-23 18:08:46 -0500

    init

--- myapp/config/initializers/secret_token.rb
+++ myapp/config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Myapp::Application.config.secret_key_base = 'e08415ad621e10bd62f68ebdc2ab0ecab68108a12f122ee5d0d8af7c447527e7070411a87f4c4996aa3a121cddd9befd3854273cb7bc72a9c8c1fd8aac3b048f'
+Myapp::Application.config.secret_key_base = 'e2174e4fada7be81cf4d682322e870dc7292aed00b759d4469fcaaaea610a45bb727f7a470bad983407b65dba6b9a51ef2f0edcec1c70657e7a7ad2ffc56ad93'
